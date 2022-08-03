84b28720695d4ea7c70d7f026d93243d6f91c33c
Umesh Kumawat <68832856+Royanosh@users.noreply.github.com>
2022-08-03 15:59:03 +0530

    Nothing New

--- todo_list_js.js
+++ todo_list_js.js
@@ -1,12 +1,12 @@
-window.addEventListener('load',() => {
-  const form  = document.querySelector("#new-task");
+window.addEventListener("load", () => {
+  const form = document.querySelector("#new-task");
   const input = document.querySelector("#new-task-input");
   const list_el = document.querySelector("#tasks");
 
-  form.addEventListener('submit', (e) =>{
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
     const task = input.value;
-    if(!task){
+    if (!task) {
       alert("Please fill out the task");
       return;
     }
@@ -20,7 +20,7 @@ window.addEventListener('load',() => {
     task_input_el.classList.add("text");
     task_input_el.type = "text";
     task_input_el.value = task;
-    task_input_el.setAttribute("readonly","readonly");
+    task_input_el.setAttribute("readonly", "readonly");
 
     task_content_el.appendChild(task_input_el);
 
@@ -40,17 +40,17 @@ window.addEventListener('load',() => {
 
     list_el.appendChild(task_el);
     input.value = "";
-    tast_action_edit_el.addEventListener('click',() =>{
-      if(tast_action_edit_el.innerText.toLowerCase()=="edit"){
+    tast_action_edit_el.addEventListener("click", () => {
+      if (tast_action_edit_el.innerText.toLowerCase() == "edit") {
         task_input_el.removeAttribute("readonly");
         task_input_el.focus();
-        tast_action_edit_el.innerText="Save";
-      }else{
-        task_input_el.setAttribute("readonly","readonly");
+        tast_action_edit_el.innerText = "Save";
+      } else {
+        task_input_el.setAttribute("readonly", "readonly");
         tast_action_edit_el.innerText = "Edit";
       }
     });
-    tast_action_delete_el.addEventListener('click',() => {
+    tast_action_delete_el.addEventListener("click", () => {
       list_el.removeChild(task_el);
     });
   });
